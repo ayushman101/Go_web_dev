22d7a1492124c2841493168e46db8cdde55ed12b
Ayushman Singh <amansinghayush@gmail.com>
2023-09-15 20:09:08 +0530

    added html templates

--- main.go
+++ main.go
@@ -5,33 +5,79 @@ import (
 	"net/http"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"html/template"
+	"log"
 )
+func logHttpHeader(r *http.Request){
 
-
-func path1(w http.ResponseWriter, r *http.Request){
-	
 	fmt.Println("Method: ",r.Method," | Proto: ", r.Proto)
 	fmt.Println("Host : ", r.Host)
-	fmt.Fprint(w,"<h1>Welcome to the server</h1>")
+}
+
+func TemplateExecute(w http.ResponseWriter, filepath string, data any){
+
+	tpl,err:=template.ParseFiles(filepath)
+	if err!=nil{
+	
+		http.Error(w,"Failed to parse response html", http.StatusInternalServerError)
+		log.Fatal(err)
+		return
+	}
+
+	err = tpl.Execute(w,data)
+
+	if err!=nil{
+
+		http.Error(w,"Failed to parse response html", http.StatusInternalServerError)
+		log.Fatal(err)
+		return
+	}
+
+
+}
+
+func path1(w http.ResponseWriter, r *http.Request){
+	logHttpHeader(r)	
+	TemplateExecute(w,"./templates/home.gohtml",nil)
+	//fmt.Fprint(w,"<h1>Welcome to the server</h1>")
 }
 
 func path2(w http.ResponseWriter, r *http.Request){
-
+	logHttpHeader(r)
 	w.Header().Set("Content-Type","text/html; charset=utf-8")
-	fmt.Fprint(w,"<h1>New Page 2</h1>")
+	
+	TemplateExecute(w,"./templates/newpage.gohtml",nil)
+
+	//fmt.Fprint(w,"<h1>New Page 2</h1>")
 }
 
 
 func userPath(w http.ResponseWriter, r *http.Request){
-
+	logHttpHeader(r)
 	userID:=chi.URLParam(r,"userID")
 
 	ctx := r.Context()
   	key := ctx.Value("userID")
 
   // respond to the client
-  w.Write([]byte(fmt.Sprintf("hi %v, %v", userID, key)))
+  //w.Write([]byte(fmt.Sprintf("hi %v, %v", userID, key)))
 	
+  user:=struct{
+	Name string
+	Userid any
+	Key any
+	Test []string
+  }{
+	Name: "John Smith",
+	Userid: userID,
+	Key: key,
+	Test: []string{
+		"red",
+		"yellow",
+	},
+  }
+
+  	TemplateExecute(w,"./templates/user.gohtml",user)
   	//fmt.Fprint(w,"<h1>User: ", userID,"</h1> <h2> Key: ",key," </h2>")
 	
 }
